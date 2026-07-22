85a95203f1d7950888a4852ed34b9a3ecebf965a
Owen <owenssrouss@gmail.com>
2026-07-22 16:19:32 -0400

    Support custom category creation in KDS and robust handleFastAdd keyword matching

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 // Service worker to make Cocina DeliverCity KDS PWA Installable
-const CACHE_NAME = 'pedigochos-v15';
+const CACHE_NAME = 'pedigochos-v16';
 const ASSETS = [
   '/kitchen.html',
   '/css/common.css',
