b6ea2a74a487c733d2b2e3aec59e215051379529
Owen <owenssrouss@gmail.com>
2026-07-24 16:21:12 -0400

    Fix menu builder filtering by syncing activeCategoryId to global window namespace

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 // Service worker to make Cocina Rapi Gochos KDS PWA Installable
-const CACHE_NAME = 'pedigochos-v20';
+const CACHE_NAME = 'pedigochos-v21';
 const ASSETS = [
   '/kitchen.html',
   '/css/common.css',
