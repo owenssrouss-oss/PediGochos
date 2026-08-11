1823869fd9ce1389882081a5a2a781f251f27d74
Owen <owenssrouss@gmail.com>
2026-08-10 23:57:47 -0400

    Completely hide disabled restaurants from all subcategories, add category emojis, and optimize full admin for 9:16 mobile viewports v123

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 // Service worker to make Rapi Gochos PWA Offline-First & Push Enabled
-const CACHE_NAME = 'pedigochos-v122';
+const CACHE_NAME = 'pedigochos-v123';
 const ASSETS = [
   '/',
   '/index.html',
